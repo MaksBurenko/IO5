f777bd59418454e737145b8000ce99aa67e8f347
Maks Burenko <Cherniymaks@yandex.ru>
2022-06-28 15:09:43 +0300

    123

--- src/IO5/Main.java
+++ src/IO5/Main.java
@@ -5,45 +5,44 @@ import java.util.Objects;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
 
-        class Animal implements Serializable {
-            private final String name;
+    class Animal implements Serializable {
+        private final String name;
 
-            public Animal(String name) {
-                this.name = name;
-            }
-
-            @Override
-            public boolean equals(Object obj) {
-                if (obj instanceof Animal) {
-                    return Objects.equals(name, ((Animal) obj).name);
-                }
-                return false;
-            }
+        public Animal(String name) {
+            this.name = name;
         }
 
-        public static Animal[] deserializeAnimalArray(byte[] data) {
-
-            ByteArrayInputStream reader = new ByteArrayInputStream(data);
-            try {
-                  for (int i=0; i < data.length; i++){
-
-                      if(i==0) {
-                        int size = reader.read();
-                          Animal [] array = new Animal[size];
-                      } else {
-                          Object o = reader.read();
-                          ObjectOutputStream os = new ObjectOutputStream(array);
-                          os.writeObject(data[i]);
-                          os.flush();
-                          os.close();
-                      }
-                  }
-
-            } catch (IllegalArgumentException | IOException e){
-                throw e;
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Animal) {
+                return Objects.equals(name, ((Animal) obj).name);
             }
+            return false;
         }
     }
+
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+
+        int size;
+        Object o;
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+            if ((size = ois.read()) != -1) {
+                Animal[] array = new Animal[size];
+                while ((o = ois.read()) != null) {
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = (Animal) o;
+                    }
+                }
+                return array;
+            }
+            ois.close();
+        } catch (IllegalArgumentException | IOException | ClassCastException e) {
+            System.out.println(e.getMessage());
+        }
+        return System.out.println(e.getMessage());
+    }
 }
+
